64fea7cee387e274229a99e4e4649df19f5d5f93
Yashu Tanna <6696871+yashutanna@users.noreply.github.com>
2024-07-28 16:24:50 +0200

    update buy function to run every 25 minutes and use an environment variable along with customerOrderIds to make sure we can customize when our orders go through  on a daily basis

--- buy-crypto.js
+++ buy-crypto.js
@@ -1,8 +1,39 @@
 const {getHeaders} = require('./auth');
+const dcaHourExecution = Number(process.env.DCA_EXECUTION_HOUR) || 15 // example: 15 (for 3 PM)
 const dcaCurrencies = process.env.DCA_CURRENCIES?.split(',').map(val => val.trim()) || [] // example: BTC,ETH
 const dcaAmounts = process.env.DCA_AMOUNTS?.split(',').map(val => val.trim()) || [] // example: 100,50
 
-async function placeBuyOrder(pair, amount, price) {
+function isDcaHour(){
+    return new Date().getHours() === dcaHourExecution;
+}
+
+function getCustomerOrderId(pair){
+    const date = new Date().getDate();
+    const month = new Date().getMonth();
+    const year = new Date().getFullYear();
+    const hour = new Date().getHours();
+    return `${pair}-${year}-${month}-${date}-${hour}`
+}
+
+async function hasAlreadyPlacedOrder(customerOrderId){
+    const order = await getOrder(customerOrderId);
+    return order.code !== -1;
+}
+
+async function getOrder(customerOrderId) {
+    const pair = customerOrderId.split('-')[0];
+    let timestamp = (new Date()).getTime();
+    const path = `/v1/orders/${pair}/customerorderid/${customerOrderId}`
+    const options = {
+        method: "GET",
+        headers: getHeaders(timestamp, "GET", path)
+    };
+    const response = await fetch(`https://api.valr.com${path}`, options);
+    const json = await response.json();
+    return json
+}
+
+async function placeBuyOrder(pair, amount, price, customerOrderId) {
     console.log(`Placing Limit Post Only Reprice order of ${amount} on pair ${pair}`);
     let timestamp = (new Date()).getTime();
     const body = {
@@ -14,7 +45,8 @@ async function placeBuyOrder(pair, amount, price) {
         "postOnlyReprice": true,
         "postOnlyRepriceTicks": "1",
         "customerOrderId": "postOnlyReprice",
-        "timeInForce": "GTC"
+        "timeInForce": "GTC",
+        customerOrderId
     };
     const options = {
         method: "POST",
@@ -23,6 +55,7 @@ async function placeBuyOrder(pair, amount, price) {
     };
     const response = await fetch('https://api.valr.com/v1/orders/limit', options);
     const json = await response.json();
+    console.log(JSON.stringify(json, null, 2))
     return json.id;
 }
 
@@ -44,19 +77,25 @@ const getAllPairs = async () => {
     return allPairs.reduce((acc, pair) => ({...acc, [pair.symbol]: pair}), {})
 }
 
-const getBestAsk = async (pair) => {
+const getMarketSummary = async (pair) => {
     let options = {
         method: "GET",
     };
     const response = await fetch(`https://api.valr.com/v1/public/${pair}/marketsummary`, options);
     const marketSummary = await response.json();
-    return marketSummary.askPrice;
+    console.log(JSON.stringify(marketSummary, null, 2));
+    return marketSummary;
 }
 
 const buy = async () => {
     try {
         if(dcaCurrencies.length !== dcaAmounts.length){
-            console.error('currencies and amounts length dont match. Please check your DCA_CURRENCIES and DCA_AMOUNTS values')
+            console.error(`currencies(${dcaCurrencies.length}) and amounts(${dcaAmounts.length}) length dont match. Please check your DCA_CURRENCIES and DCA_AMOUNTS values`)
+            return;
+        }
+        if(!isDcaHour()){
+            console.log(`not running DCA as current hour(${new Date().getHours()}) is not DCA_EXECUTION_HOUR(${dcaHourExecution})`);
+            return;
         }
         const fiatCurrency = 'ZAR'
         const balances = await getBalances();
@@ -80,14 +119,22 @@ const buy = async () => {
                 console.log(`dcaAmount(${dcaAmounts[index]}) too low. require minimum purchase of(${pairs[pair].minQuoteAmount})${fiatCurrency}`);
                 return;
             }
-            const bestAsk = await getBestAsk(pair)
-            const amountToBuy = Number(dcaAmounts[index]) / Number(bestAsk);
+            const customerOrderId = getCustomerOrderId(pair)
+            if(await hasAlreadyPlacedOrder(customerOrderId)){
+                // do nothing if this order has already been placed for the day
+                console.log(`customerOrderId(${customerOrderId}) already exists`);
+                return;
+            }
+
+            const marketSummary = await getMarketSummary(pair)
+            const price = marketSummary.askPrice;
+            const amountToBuy = Number(dcaAmounts[index]) / Number(price);
             if(amountToBuy < pairs[pair].minBaseAmount){
                 // do nothing if there is not sufficient balance
                 console.log(`amountToBuy(${amountToBuy}) too low. require minimum base amount(${pairs[pair].minBaseAmount})`);
                 return;
             }
-            return placeBuyOrder(pair, amountToBuy, bestAsk);
+            return placeBuyOrder(pair, amountToBuy, price, customerOrderId);
         });
 
         const orderIds = await Promise.all(buyPromises);
